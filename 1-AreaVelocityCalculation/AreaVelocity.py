25db9cc03bd8c4b10dda22fd76a66f273f8b9f88
stepinski <stepinski@gmail.com>
2022-05-17 13:22:34 +0200

    refactoring to dataframes

--- 1-AreaVelocityCalculation/AreaVelocity.py
+++ 1-AreaVelocityCalculation/AreaVelocity.py
@@ -1,42 +1,57 @@
 import csv
+from multiprocessing.spawn import prepare
 import sys
 import math
 import pandas as pd
 # Tesler script to calculate the area of a partially full pipe, multiply by velocity to produce a flow
+def prepare_data(data, data_name: str):
+    """ datetime type """
+    data["time"] = pd.to_datetime(data.time)
+
+    """ sort values """
+    data.sort_values(by=['time'], inplace=True)
+
+    """ set index """
+    data.set_index("time", inplace=True)
+
+    """ drop duplicate time"""
+    data = data[~data.index.duplicated(keep='first')]
+
+    """ resample """
+    data = data.resample('5T').pad()
+
+    """ reset index """
+    data.reset_index("time", inplace=True)
+
+    """ rename column names """
+    data.columns = ['time', data_name]
+
+    # data[data_name][data[data_name]==0] = np.nan
+    return data
+
+
+def merge_data(channel1, channel2):
+    data = pd.merge_ordered(channel1, channel2, on=['time'], how='outer')
+    data.drop_duplicates(subset="time", inplace=True)
+    return data
+
 
 diameter=900.0 # (diameter of pipe in mm)
-
-dateData=[]
-depths=[]
-velocities=[]
-flows=[]
-
-depths = pd.read_csv("level.csv")
-vels = pd.read_csv("velocity.csv")
-# with open("Level.csv") as csvfile:
-#     reader=csv.reader(csvfile,delimiter=',')
-#     next(reader)
-#     for row in reader:
-#        dateData.append(str(row[0]))
-#        depths.append(float(row[1]))
-
-# with open("Velocity.csv") as csvfile:
-#     reader2=csv.reader(csvfile,delimiter=',')
-#     next(reader2)
-#     for row in reader2:
-#         velocities.append(float(row[1]))
-
-if len(depths)!=len(velocities):
-      sys.exit()
-
 diameterM=diameter/1000.0 # calculate diameter in m
 radius=diameter/2.0 # calculate radius in mm
 radiusM=radius/1000.0 # calculate radius in m
-
 area=math.pi*(diameterM)**2/4 # calculate area of full pipe in square meters
 
-for index,depth in enumerate(depths):
-        velocity=velocities[index]
+dateData=[]
+flows=[]
+
+dp = pd.read_csv("level.csv")
+dp = prepare_data(dp,"depth")
+
+vel = pd.read_csv("velocity.csv")
+vel = prepare_data(vel,"velocity")
+
+def calcFlow(depth, velocity):
         p=abs(radius-depth) # length from centre of pipe to water surface
         thetaO=math.acos(p/radius) # angle between centre of pipe and where water touches side of pipe
         alphaA=thetaO*2.0 # twice the subtended angle thetaO
@@ -47,15 +62,14 @@ for index,depth in enumerate(depths):
                 areaC=areaSector-areaMon # calculate area of pipe flow when pipe is less than or equal to half full
         else:
                 areaC=area-(areaSector-areaMon) # calculate area of pipe flow when pipe is greater than half full
-
         flow=areaC*(velocity/3.281)*1000.0 # calculate flow in l/s
-        flows.append(flow)
+                
 
-f=open("Calculated Flow.csv","a+",newline='\n') 
-f.write("time,value\n")
+df = merge_data(dp, vel)
 
-for n in range(0,len(dateData)):
-        outString=dateData[n]+","+str(flows[n])+"\n"
-        f.write(outString)
-        print(outString)
-f.close()
+df['flow'] = df.apply(lambda row: calcFlow(row.depth,row.velocity), axis = 1)
+df= df[['time','flow']]
+df.columns = ['time', 'value']
+df.set_index("time", inplace=True)
+output_file_name="Calculated Flow"
+df.to_csv(output_file_name + ".csv")
